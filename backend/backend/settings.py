66cbaf0b595d4283a72e864e5fa19b0ce9ff1011
hagarhaytham <hagar.bahy97@eng-st.cu.edu.eg>
2024-02-12 01:39:31 +0200

    edit settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +140,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Replace with your desired path
 
+django_heroku.settings(locals())
